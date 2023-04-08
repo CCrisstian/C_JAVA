b9fc0f152abeb6af5ce1b16076bfec3fcdda670b
CCrisstian <crisstiann.c@gmail.com>
2023-04-07 22:39:28 -0300

    Matrices

--- HelloWorld/src/EjemploMatrices.java
+++ HelloWorld/src/EjemploMatrices.java
@@ -2,40 +2,42 @@ import javax.swing.*;
 
 public class EjemploMatrices {
 
-    public static String mostrarMatriz(int[][] matriz) {
-        String m = "";
-        for (int i = 0; i < matriz.length; i++) {
-            for (int j = 0; j < matriz[i].length; j++) {
-                m = m + (matriz[i][j]) + "\t ";
-            }
-            m = m + "\n";
-        }
-        return m;
-    }
-
     public static void main(String[] args) {
 
         int[][] números = new int[2][4];
 
-        for (int i = 0; i < números.length; i++) {
-            for (int j = 0; j < números[i].length; j++) {
-                try {
-                    números[i][j] = Integer.parseInt(JOptionPane.showInputDialog("Ingrese un número para la matriz"));
-                }
-                catch (NumberFormatException e){
-                    JOptionPane.showMessageDialog(null,"Ingreso un valor incorrecto, por favor ingrese un número entero");
-                    j--;
-                }
-            }
-        }
+        números[0][0] = 1;
+        números[0][1] = 2;
+        números[0][2] = 3;
+        números[0][3] = 4;
 
-        String mensaje = "Matriz a \n";
-        mensaje = mensaje + mostrarMatriz(números);
-        mensaje = mensaje + "Cantidad de filas --> " + números.length +"\n";
-        mensaje = mensaje + "Cantidad de columnas --> " + números[0].length + "\n";
-        mensaje = mensaje + "El PRIMER elemento de la matriz es --> " + números[0][0] + "\n";
-        mensaje = mensaje + "El ULTIMO elemento de la matriz es --> " + números[números.length-1][números[1].length-1];
-        JOptionPane.showMessageDialog(null,mensaje);
+        números[1][0] = 11;
+        números[1][1] = 12;
+        números[1][2] = 13;
+        números[1][3] = 14;
+
+        System.out.println("PRIMER elemento de la matriz --> "+números[0][0]);
+        System.out.println("ULTIMO elemento de la matriz --> "+números[números.length-1][números[1].length-1]);
+        System.out.println("Número de filas "+números.length);
+        System.out.println("Número de columnas "+números[0].length);
+
+        int num1 = números[0][0];
+        int num2 = números[0][1];
+        int num3 = números[0][2];
+        int num4 = números[0][3];
+        int num5 = números[1][0];
+        int num6 = números[1][1];
+        int num7 = números[1][2];
+        int num8 = números[1][3];
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+        System.out.println("num3 = " + num3);
+        System.out.println("num4 = " + num4);
+        System.out.println("num5 = " + num5);
+        System.out.println("num6 = " + num6);
+        System.out.println("num7 = " + num7);
+        System.out.println("num8 = " + num8);
+
 
     }
 }
