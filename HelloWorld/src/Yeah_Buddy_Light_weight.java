92934d1bafeac3af59a216e2703e10ca90e4fc95
CCrisstian <111469216+CCrisstian@users.noreply.github.com>
2023-04-13 14:11:28 -0300

    Yeah Buddy Light weight 13/4

---
+++ HelloWorld/src/Yeah_Buddy_Light_weight.java
@@ -0,0 +1,29 @@
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.swing.*;
+
+public class Yeah_Buddy_Light_weight {
+    public static void main(String[] args) throws Exception {
+        // Cargar el archivo de sonido
+        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(
+                Yeah_Buddy_Light_weight.class.getResourceAsStream("SoundEffect.wav"));
+
+        // Crear un clip de sonido y abrir el flujo de audio
+        Clip clip = AudioSystem.getClip();
+        clip.open(audioInputStream);
+
+        // Reproducir el sonido
+        clip.start();
+
+        // Mostrar un mensaje por pantalla
+        JOptionPane.showMessageDialog(null,"Yeah Buddy!\nLight weight");
+        // Esperar a que termine el sonido
+        Thread.sleep(clip.getMicrosecondLength() / 1000);
+
+        // Cerrar el clip de sonido y el flujo de audio
+        clip.stop();
+        clip.close();
+        audioInputStream.close();
+    }
+}
